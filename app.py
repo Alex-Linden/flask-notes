21504afb0badecfd6365b36c8e15ed09f9e17021
Alex Linden <alextl2004@gmail.com>
2022-06-21 12:11:46 -0700

    base forms

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, redirect, session, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, db, User
+from forms import RegisterForm, LoginForm, CSRFProtectForm
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///hashing_login"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "abc123"
+
+connect_db(app)
+db.create_all()
+
+toolbar = DebugToolbarExtension(app)
+
+
+@app.get("/")
+def homepage():
+    """rederict to register."""
+
+    return redirect("/register")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user: produce form & handle form submission."""
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        name = form.username.data
+        pwd = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.first_name.data
+
+        user = User.register(name, pwd, email, first_name, last_name)
+        db.session.add(user)
+        db.session.commit()
+
+        session["user_id"] = user.id
+
+        # on successful login, redirect to secret page
+        return redirect("/secret")
+
+    else:
+        return render_template("register.html", form=form)
+
